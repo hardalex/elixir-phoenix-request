1345dc3b0db59bffce06a169dbaf6a36d5b43a73
Olexandr Diacheko <olexandr.diachenko@gmail.com>
2016-12-14 12:29:15 +0200

    Update request.ex

--- request.ex
+++ request.ex
@@ -1,22 +1,22 @@
 defmodule Common.Request do
-	def json(url) do
-		data = nil
-		case HTTPoison.get(url) do
-			{:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-				{:ok, data} = Poison.decode(body)
-			{:error, %HTTPoison.Error{reason: reason}} ->
-				data = %{error: reason}
-		end
-		data
-	end
-	def json(url, body) do
-		data = nil    
-		case HTTPoison.post(url, body, [{"Accept", "application/json"}, {"Content-Type", "application/json"}]) do
-			{:ok, %HTTPoison.Response{status_code: _, body: body}} ->
-				{:ok, data} = Poison.decode(body)
-			{:error, %HTTPoison.Error{reason: reason}} ->
-				data = %{error: reason}
-		end
-		data
-	end
+    def json(url) do
+        {:ok, data} =
+            case HTTPoison.get(url) do
+                {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+                    Poison.decode(body)
+                {:error, %HTTPoison.Error{reason: reason}} ->
+                    {:ok, %{error: reason}}
+            end
+        data
+    end
+    def json(url, body) do
+        {:ok, data} =
+            case HTTPoison.post(url, body, [{"Accept", "application/json"}, {"Content-Type", "application/json"}]) do
+                {:ok, %HTTPoison.Response{status_code: _, body: body}} ->
+                    Poison.decode(body)
+                {:error, %HTTPoison.Error{reason: reason}} ->
+                    {:ok, %{error: reason}}
+            end
+        data
+    end
 end
